08b20e84975d97346bf5e24ecf8422035307364c
EcoBay <ecobayod011903@gmail.com>
2022-02-06 17:27:16 +0800

    Seperated WiFi functions from main
    Move WiFi functions to wifi.c and also remove the event handler for getting connected. Also retained handler for WiFi disconnected event

---
+++ main/include/wifi.h
@@ -0,0 +1,9 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#define WIFI_SSID       CONFIG_ESP_WIFI_SSID
+#define WIFI_PASSWORD   CONFIG_ESP_WIFI_PASSWORD
+
+void start_wifi(void);
+
+#endif
---
+++ main/wifi.c
@@ -0,0 +1,64 @@
+#include "wifi.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define WIFI_CONNECTED_BIT  BIT0
+#define WIFI_FAIL_BIT       BIT1
+
+static const char* TAG = "WiFi";
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base != WIFI_EVENT) return;
+    if (event_id == WIFI_EVENT_STA_DISCONNECTED || event_id == WIFI_EVENT_STA_START) {
+        if (event_id == WIFI_EVENT_STA_DISCONNECTED)
+            ESP_LOGI(TAG, "Disconnected");
+        esp_wifi_connect();
+    }
+}
+
+void start_wifi(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+            ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();;
+
+    const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        NULL));
+    wifi_config_t wcfg = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+            .pmf_cfg = {
+                .capable = true,
+                .required = false,
+            }
+        }
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wcfg));
+    ESP_LOGI(TAG, "WiFi init done");
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
